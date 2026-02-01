b06679c28a9a2c1115b55e7f394533301f5ed10c
Vivek Chaurasiya <chaurasiyavivek658@gmail.com>
2026-02-01 21:26:41 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  server: {
+    https: false
+  }
 })
